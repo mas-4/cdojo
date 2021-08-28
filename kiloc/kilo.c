162b031d89318fc69ad5c4eef848759e9e75dbd2
mas <m@stdwtr.io>
2021-08-28 13:40:32 -0400

    Fixed
    I have no idea what was wrong.

--- kiloc/kilo.c
+++ kiloc/kilo.c
@@ -321,9 +321,9 @@ void editorDrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
   int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-      E.filename ? E.filename : "[No Name]", E.numrows);
-  int rlen = snprintf(status, sizeof(rstatus), "%d/%d",
-      E.cy + 1, E.numrows);
+    E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+    E.cy + 1, E.numrows);
   if (len > E.screencols) len = E.screencols;
   abAppend(ab, status, len);
   while (len < E.screencols) {
